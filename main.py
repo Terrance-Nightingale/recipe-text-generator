4fc73aa0911e058dffdbdade19b7152059af3fb2
Terrance-Nightingale <130939937+Terrance-Nightingale@users.noreply.github.com>
2023-10-13 14:17:28 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+# TODO Set up Sinch API for SMS messaging
+# TODO Pull recipes from recipe API (or from ChatGPT)
+# TODO Compile recipes into SMS message to send to user
+# TODO Connect to Windows Task Manager to automate process every Friday at noon
+
+SINCH_API = "https://us.sms.api.sinch.com"
